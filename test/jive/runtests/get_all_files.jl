6378ad93b20945f4e4d9995df1f68b272dbb3c15
WooKyoung Noh <wookay.noh@gmail.com>
2020-08-08 17:10:14 +0900

    fix #26, get_all_files

--- test/jive/runtests/get_all_files.jl
+++ test/jive/runtests/get_all_files.jl
@@ -3,10 +3,16 @@ module test_jive_runtests_get_all_files
 using Test
 using Jive
 
-dir = normpath(@__DIR__, "..")
+dir = normpath(@__DIR__, "..") # Jive/test/jive/
 skip = []
 targets = ["//./pipe", "USE_REVISE", "USE_PLOTPANE", "runtests/get_all_files"]
 (all_files, start_idx) = Jive.get_all_files(dir, skip, targets)
 @test all_files == ["runtests/get_all_files.jl"]
+
+dir = normpath(@__DIR__, ".") # Jive/test/jive/runtests/
+skip = []
+targets = ["//./pipe", "USE_REVISE", "USE_PLOTPANE", "get_all_files"]
+(all_files, start_idx) = Jive.get_all_files(dir, skip, targets)
+@test all_files == ["get_all_files.jl"]
 
 end # module test_jive_runtests_get_all_files
