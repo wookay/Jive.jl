698c102825a95cb827c19d0be7783f018602af5c
WooKyoung Noh <wookay.noh@gmail.com>
2021-06-10 15:12:18 +0900

    broken/skip keyword argument

--- test/test/broken.jl
+++ test/test/broken.jl
@@ -5,4 +5,9 @@ using Test
 @test_broken 1 == 2
 @test_skip   1 == 2
 
+if VERSION >= v"1.7.0-DEV.865"  # Julia PR #39322
+@test 1 == 2 broken=true
+@test 1 == 2 skip=true
+end
+
 end # module test_test_broken
