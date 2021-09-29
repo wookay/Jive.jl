9a67c81be88119b5e5b473cc6b0afdbb788aa6f1
WooKyoung Noh <wookay.noh@gmail.com>
2021-09-29 12:00:28 +0900

    test default context

--- test/jive/runtests/context.jl
+++ test/jive/runtests/context.jl
@@ -8,6 +8,9 @@ context_variable = 1
 runtests(@__DIR__, targets=["target1"], enable_distributed=false, context=nothing)
 @test context_variable == 1
 
+runtests(@__DIR__, targets=["target1"], enable_distributed=false) # default context=nothing
+@test context_variable == 1
+
 runtests(@__DIR__, targets=["target1"], enable_distributed=false, context=@__MODULE__)
 @test context_variable == 3
 
