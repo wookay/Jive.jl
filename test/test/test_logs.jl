2b3a46d7a16e3216105ae1c3c392c119a8df7531
WooKyoung Noh <wookay.noh@gmail.com>
2022-02-12 21:27:19 +0900

    test testlogger

--- test/test/test_logs.jl
+++ test/test/test_logs.jl
@@ -12,3 +12,20 @@ end
 @test (@test_logs (:info, "bar") (:info, "baz") f()) === :ok
 
 end # module test_test_logs
+
+
+using Jive
+@If VERSION >= v"1.8.0-DEV.1493" module test_testlogger
+
+using Test, Logging
+
+logger = TestLogger()
+with_logger(logger) do
+    @info 1493
+end
+
+l = first(logger.logs)
+@test l isa LogRecord
+@test l.message == 1493
+
+end # module test_testlogger
