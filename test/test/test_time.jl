9e2488ee991147627b97c23480d45b3bbdd65b57
WooKyoung Noh <wookay.noh@gmail.com>
2022-07-20 02:10:13 +0900

    Jive v0.2.27

--- test/test/test_time.jl
+++ test/test/test_time.jl
@@ -2,10 +2,12 @@ module test_test_time
 
 using Test
 
-redirect_stdout(devnull) do
-    @time :ok
-    @time :ok
-    @time :ok
+if VERSION >= v"1.6"
+    redirect_stdout(devnull) do
+        @time :ok
+        @time :ok
+        @time :ok
+    end
 end
 
 end # module test_test_time
