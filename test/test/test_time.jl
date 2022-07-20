1c2acd90c7db2a0ab5eebb2bda34602210a01b5a
WooKyoung Noh <wookay.noh@gmail.com>
2022-07-21 02:00:54 +0900

    test time, timev macro

--- test/test/test_time.jl
+++ test/test/test_time.jl
@@ -3,11 +3,12 @@ module test_test_time
 using Test
 
 if VERSION >= v"1.6"
-    redirect_stdout(devnull) do
-        @time :ok
-        @time :ok
-        @time :ok
+    redirect_stdout(stdout) do
+        @time  :ok
+        @timev :ok
     end
 end
+
+@test true
 
 end # module test_test_time
