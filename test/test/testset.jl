ede2049c2c42b3e28ddaf3c2f8287f848cd478cd
WooKyoung Noh <wookay.noh@gmail.com>
2025-03-24 07:25:24 +0900

    test testset results

--- test/test/testset.jl
+++ test/test/testset.jl
@@ -46,10 +46,66 @@ end # module test_testset_42518
 module test_testset_let
 
 using Test
-using Jive
 
 @testset let (fname, events) = ("fpath", [])
     @test fname == "fpath"
 end
 
 end # module test_testset_let
+
+
+module test_testset_results
+
+using Test
+
+# from julia/stdlib/Test/test/nothrow_testset.jl
+mutable struct NoThrowTestSet <: Test.AbstractTestSet
+    results::Vector
+    NoThrowTestSet(desc) = new([])
+end
+Test.record(ts::NoThrowTestSet, t::Test.Result) = (push!(ts.results, t); t)
+Test.finish(ts::NoThrowTestSet) = ts.results
+
+module M
+    n = 42
+end
+
+let results = @testset NoThrowTestSet begin
+    @test isconst(M, :zero) # Pass   :test
+    @test isconst(M, :n)    # Fail   :test
+    @test isconst(42)       # Error  :test_error
+    @test 42                # Error  :test_nonbool
+    @test_broken 1 == 2     # Broken :test
+    @test_skip   1 == 2     # Broken :skipped
+end
+
+result = popfirst!(results)
+@test result isa Test.Pass
+@test result.test_type === :test
+@test result.value == true
+
+result = popfirst!(results)
+@test result isa Test.Fail
+@test result.test_type === :test
+@test result.value == "false"
+
+result = popfirst!(results)
+@test result isa Test.Error
+@test result.test_type === :test_error
+@test startswith(result.value, "MethodError(isconst, (42,), ")
+
+result = popfirst!(results)
+@test result isa Test.Error
+@test result.test_type === :test_nonbool
+@test result.value == "42"
+
+result = popfirst!(results)
+@test result isa Test.Broken
+@test result.test_type === :test
+
+result = popfirst!(results)
+@test result isa Test.Broken
+@test result.test_type === :skipped
+
+end # let results = @testset NoThrowTestSet
+end # module test_testset_results
