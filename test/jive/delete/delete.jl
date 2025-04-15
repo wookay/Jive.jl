b4a4b983af1e156b18c5d1d4d54a71f90d7dc7ce
WooKyoung Noh <wookay.noh@gmail.com>
2025-04-16 06:41:20 +0900

    more test Jive.delete

--- test/jive/delete/delete.jl
+++ test/jive/delete/delete.jl
@@ -13,12 +13,17 @@ Jive.delete(f, Tuple{Int})
 
 f(::Int) = 42
 @test_throws ArgumentError("Collection is empty, must contain exactly 1 element") Jive.delete(f)
-@test_throws ArgumentError("Collection is empty, must contain exactly 1 element") Jive.delete(f, Tuple{})
 @test_throws MethodError(f, (), Base.get_world_counter()) f()
 @test f(0) == 42
 Jive.delete(f, Tuple{Int})
 
 f(::Int, ::String) = 42
+f(::Int, ::Int) = 42
+f(::Int, ::Any) = 42
+@test_throws ArgumentError("Collection has multiple elements, must contain exactly 1 element") Jive.delete(f, Tuple{Int, Any})
 Jive.delete(f, Tuple{Int, String})
+@test_throws ArgumentError("Collection has multiple elements, must contain exactly 1 element") Jive.delete(f, Tuple{Int, Any})
+Jive.delete(f, Tuple{Int, Int})
+Jive.delete(f, Tuple{Int, Any})
 
 end # module test_jive_delete
