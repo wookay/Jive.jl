38f12c38c251e5bc54f23d375ba1bbb5fe0fb9f0
WooKyoung Noh <wookay.noh@gmail.com>
2025-09-02 20:02:30 +0900

    test ScopedValue, LazyScopedValue

---
+++ test/test/scoped_values.jl
@@ -0,0 +1,19 @@
+using Jive
+@If VERSION >= v"1.13.0-DEV.1070" module test_scoped_values # julia commit 5fb39c695e93d5f13a95d57ffa4c537a79f11b58
+
+using Test
+
+# Test.TESTSET_PRINT_ENABLE
+# julia 1.13.0-DEV.1044
+# bb368512880ca6cf051a91993c9c5bb4a8d3b7d0
+
+# Test.TEST_RECORD_PASSES
+# julia 1.13.0-DEV.1070
+# 5fb39c695e93d5f13a95d57ffa4c537a79f11b58
+
+@Base.ScopedValues.with Test.TESTSET_PRINT_ENABLE=>false Test.TEST_RECORD_PASSES=>false begin
+    @test Test.TESTSET_PRINT_ENABLE[] === false
+    @test Test.TEST_RECORD_PASSES[] === false
+end
+
+end # module test_scoped_values
