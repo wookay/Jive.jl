50f66c67e2e2f96495f1f03922b05c9171b5d1f8
WooKyoung Noh <wookay.noh@gmail.com>
2026-01-19 10:07:35 +0900

    run test_warn for DaemonMode

--- test/test/test_warn.jl
+++ test/test/test_warn.jl
@@ -2,12 +2,23 @@ module test_test_warn_nowarn
 
 using Test
 
-@test_warn   "warning" println(stderr, :warning)
+function test_warnings()
+    @test_warn "warning" println(stderr, :warning)
 
-if VERSION >= v"1.8.0-DEV.363"
-@test_warn   "warning" (@warn :warning)
+    if VERSION >= v"1.8.0-DEV.363"
+        @test_warn "warning" (@warn :warning)
+    end
+
+    @test_nowarn (1 + 2)
 end
 
-@test_nowarn           (1 + 2)
+
+if stderr isa Base.TTY
+    test_warnings()
+else
+    using Base.CoreLogging: ConsoleLogger, with_logger
+    logger = ConsoleLogger()
+    with_logger(test_warnings, logger)
+end
 
 end # module test_test_warn_nowarn
