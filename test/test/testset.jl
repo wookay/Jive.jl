b223fc00981e189f765042fd6c123d86fe719f47
WooKyoung Noh <wookay.noh@gmail.com>
2026-07-23 22:01:44 +0900

    test `@testset verbose = true`

--- test/test/testset.jl
+++ test/test/testset.jl
@@ -3,3 +3,16 @@ using Test
 @testset "basic" begin
     @test 3 == 1+2
 end
+
+# from julia/stdlib/Test/test/runtests.jl
+@testset "Child 1" verbose = true begin
+@testset "Child 1.1 (long name)" begin
+    @test 1 == 1
+end
+end
+
+@testset "Child 2" begin
+@testset "Child 2.1" begin
+    @test 1 == 1
+end
+end
